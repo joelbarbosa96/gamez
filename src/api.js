b23de9b3be19f84eb20d70c5ad508d96c03444f8
Joel Barbosa <joelbarbosa@gmail.com>
2021-01-18 11:33:12 +0100

    Redux setup

---
+++ src/api.js
@@ -0,0 +1,35 @@
+//Base URL
+const BASE_URL = "https://api.rawg.io/api/";
+
+//Getting current month
+const getCurrentMonth = () => {
+  const month = new Date().getMonth() + 1;
+  if (month < 10) {
+    return `0${month}`;
+  } else {
+    return month;
+  }
+};
+
+//Getting current day
+const getCurrentDay = () => {
+  const day = new Date().getDate();
+  if (day < 10) {
+    return `0${day}`;
+  } else {
+    return day;
+  }
+};
+
+//Getting current day/month/year
+const currentYear = new Date().getFullYear();
+const currentMonth = getCurrentMonth();
+const currentDay = getCurrentDay();
+const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
+const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
+const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
+
+//Popular Games
+const popularGames = `games?dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+
+export const popularGamesURL = () => `${BASE_URL}${popularGames}`;
